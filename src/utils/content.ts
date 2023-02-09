60e16249a2f6a59d6fe789dd3189857148620bb4
Viktor <minkovvs@gmail.com>
2023-02-09 03:11:58 +0300

    refactor: refactor files

--- src/utils/content.ts
+++ src/utils/content.ts
@@ -178,7 +178,7 @@ export const content = {
             en: 'My reviews',
             ru: 'Мои отзывы',
         },
-        favourites: {
+        favorites: {
             en: 'My favorite restaurants',
             ru: 'Избранное',
         },
